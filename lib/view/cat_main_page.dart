c61160774af3ac8f3c80828151f0cd8f82499a2a
Dastan <dastan.tulokulov@gmail.com>
2022-04-27 18:41:42 +0600

    added scrollablity

--- lib/view/cat_main_page.dart
+++ lib/view/cat_main_page.dart
@@ -23,111 +23,114 @@ class _CatMainPageState extends State<CatMainPage> {
         centerTitle: true,
         title: const Text('Cats'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            const SizedBox(height: 16),
-            FutureBuilder<List<Cat>>(
-              future: WebService().getCatFact(),
-              builder: (context, snapshot) {
-                if (kDebugMode) {
-                  print('snapshot.error');
-                  print(snapshot.error);
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasData &&
-                    snapshot.data!.isNotEmpty &&
-                    snapshot.connectionState == ConnectionState.done) {
-                  try {
-                    return ListView.separated(
-                      itemBuilder: (BuildContext context, int index) {
-                        final item = snapshot.data![index];
-                        Hive.box(HiveBoxes.fact).add(item.text);
-                        dPrint(item, 'cats item');
-                        return Column(
-                          children: [
-                            Text(
-                              item.text,
-                              style: const TextStyle(color: Colors.green),
-                            ),
-                          ],
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const SizedBox(height: 8);
-                      },
-                      itemCount: snapshot.data?.length ?? 0,
-                    );
-                  } on Exception catch (e) {
-                    dPrint(e, 'Error');
-                    return Center(
-                      child: Text('Something went wrong $e'),
-                    );
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              FutureBuilder<List<Cat>>(
+                future: WebService().getCatFact(),
+                builder: (context, snapshot) {
+                  if (kDebugMode) {
+                    print('snapshot.error');
+                    print(snapshot.error);
                   }
-                } else {
-                  const Text('Something went wrong');
-                }
-                return const Center(
-                    child: CircularProgressIndicator.adaptive());
-              },
-            ),
-            const SizedBox(height: 16),
-            Image.network('https://cataas.com/cat', loadingBuilder:
-                ((BuildContext context, Widget child,
-                    ImageChunkEvent? loadingProgress) {
-              if (loadingProgress == null) return child;
-              return Center(
-                  child: CircularProgressIndicator(
-                value: loadingProgress.expectedTotalBytes != null
-                    ? loadingProgress.cumulativeBytesLoaded /
-                        loadingProgress.expectedTotalBytes!
-                    : null,
-              ));
-            })),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: double.maxFinite,
-              child: ElevatedButton(
-                  onPressed: () {
-                    WebService().getCatFact();
-                  },
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.green),
-                      elevation: MaterialStateProperty.all<double>(0),
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                          const EdgeInsets.all(16)),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(22)),
-                      )),
-                  child: const Text('Another fact!')),
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: double.maxFinite,
-              child: ElevatedButton(
-                  onPressed: () async {
-                    await Hive.openBox<dynamic>(HiveBoxes.fact);
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasData &&
+                      snapshot.data!.isNotEmpty &&
+                      snapshot.connectionState == ConnectionState.done) {
+                    try {
+                      return ListView.separated(
+                        itemBuilder: (BuildContext context, int index) {
+                          final item = snapshot.data![index];
+                          Hive.box(HiveBoxes.fact).add(item.text);
+                          dPrint(item, 'cats item');
+                          return Column(
+                            children: [
+                              Text(
+                                item.text,
+                                style: const TextStyle(color: Colors.green),
+                              ),
+                            ],
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return const SizedBox(height: 8);
+                        },
+                        itemCount: snapshot.data?.length ?? 0,
+                      );
+                    } on Exception catch (e) {
+                      dPrint(e, 'Error');
+                      return Center(
+                        child: Text('Something went wrong $e'),
+                      );
+                    }
+                  } else {
+                    const Text('Something went wrong');
+                  }
+                  return const Center(
+                      child: CircularProgressIndicator.adaptive());
+                },
+              ),
+              const SizedBox(height: 16),
+              Image.network('https://cataas.com/cat', loadingBuilder:
+                  ((BuildContext context, Widget child,
+                      ImageChunkEvent? loadingProgress) {
+                if (loadingProgress == null) return child;
+                return Center(
+                    child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes!
+                      : null,
+                ));
+              })),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                    onPressed: () {
+                      WebService().getCatFact();
+                    },
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.green),
+                        elevation: MaterialStateProperty.all<double>(0),
+                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            const EdgeInsets.all(16)),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(22)),
+                        )),
+                    child: const Text('Another fact!')),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                    onPressed: () async {
+                      await Hive.openBox<dynamic>(HiveBoxes.fact);
 
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (_) => const FactHistory()));
-                  },
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.blue),
-                      elevation: MaterialStateProperty.all<double>(0),
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                          const EdgeInsets.all(16)),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(22)),
-                      )),
-                  child: const Text('Fact history')),
-            ),
-          ],
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (_) => const FactHistory()));
+                    },
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue),
+                        elevation: MaterialStateProperty.all<double>(0),
+                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            const EdgeInsets.all(16)),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(22)),
+                        )),
+                    child: const Text('Fact history')),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
